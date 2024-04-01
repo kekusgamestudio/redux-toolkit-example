4bc860ef57aeb8b4b790e2ca94c7f771a0b3f919
Sergio Martino <sergio.martino@modo.com.ar>
2024-04-01 17:46:40 -0300

    fetching pokemonapi info using Axios

--- src/PokemonApp.tsx
+++ src/PokemonApp.tsx
@@ -1,28 +1,45 @@
 import { useEffect } from "react"
-import { useDispatch } from "react-redux"
-import { getPokemons } from "./store/slices/pokemon";
+import { useDispatch, useSelector } from "react-redux"
+import { PokemonData, getPokemons } from "./store/slices/pokemon";
+import { RootState } from './store/store'
 
 export const PokemonApp = () => {
 
+
+  const { pokemons = [], isLoading, page } = useSelector((state: RootState) => state.pokemons)
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch( getPokemons() );
   }, []);
   
-
   return (
     <>
       <h1>
         Pokemon App
       </h1>
       <hr />
+      <span>Loading: { isLoading ? 'True' : 'False' }</span>
       <ul>
-        <li>Hola</li>
-        <li>Hola</li>
-        <li>Hola</li>
+        {
+          pokemons.map((pokemon: PokemonData) => {
+            return <li key={pokemon.name}>{pokemon.name}</li>
+          })
+        }
       </ul>
+      <hr />
+      <h3>Página: {page}</h3>
+      <div className="card">
+        <button 
+          onClick={() => dispatch( getPokemons(page-2) )}
+        >
+          Previous Page
+        </button>
+        <button onClick={() => dispatch( getPokemons(page) )}>
+          Next Page
+        </button>
+      </div>
     </>
-    
   )
 }
+
